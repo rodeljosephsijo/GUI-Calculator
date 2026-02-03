a50f7d70740bbf200088029fdf07279af290365f
Rodel Joseph Sijo <s.rodeljoseph@gmail.com>
2026-02-03 20:53:31 +0530

    Add display labels

--- Calculator_code.py
+++ Calculator_code.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 
 LIGHT_GREY="#F5F5F5"
+LABEL_COLOUR = "#25265E"
+SMALL_FONT_STYLE= ("Arial", 16)
+LARGE_FONT_STYLE= ("Arial", 40,"bold")
 class Calculator:
     def __init__(self):
         self.window = tk.Tk()
@@ -8,8 +11,23 @@ class Calculator:
         self.window.resizable(0, 0)
         self.window.title("Project_1 Calculator")
 
+        self.total_expression = "0"
+        self.current_expression= "0"
+
         self.display_frame = self.create_display_frame()
         self.buttons_frame = self.create_buttons_frame()
+
+        self.total_label, self.labels= self.create_display_labels()
+    def create_display_labels(self):
+        total_label = tk.Label(self.display_frame, text=self.total_expression, anchor=tk.E,
+                                bg=LIGHT_GREY, fg=LABEL_COLOUR, padx=24, font=SMALL_FONT_STYLE)
+        total_label.pack(expand=True, fill="both")
+
+        label = tk.Label(self.display_frame, text=self.current_expression, anchor=tk.E,
+                                bg=LIGHT_GREY, fg=LABEL_COLOUR, padx=24, font=LARGE_FONT_STYLE)
+        label.pack(expand=True, fill="both")
+
+        return total_label, label
 
     def create_display_frame(self):
         frame=tk.Frame(self.window, height=221, bg=LIGHT_GREY)
